7b34c9849fc7f2e68dbd068f4f3b01bb0eb0fd62
andy <andy870622@gmail.com>
2020-10-06 03:00:01 +0800

    Initial Commit

---
+++ drawing/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  drawing
+//
+//  Created by User15 on 2020/10/6.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ drawing/drawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  drawingApp.swift
+//  drawing
+//
+//  Created by User15 on 2020/10/6.
+//
+
+import SwiftUI
+
+@main
+struct drawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
